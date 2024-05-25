da68248cc6745dc9ddc513c00a635ba685b53aae
Waffentracher <142826622+Waffentracher@users.noreply.github.com>
2024-05-25 19:47:20 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,29 @@
 import { defineConfig } from 'vite';
-import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig(({ command }) => {
   return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
     root: 'src',
     build: {
       sourcemap: true,
       rollupOptions: {
-        input: glob.sync('src/*.html'),
+        input: {
+          main: './src/index.html',
+          snackbar: './src/2-snackbar.html',
+          timer: './src/1-timer.html',
+        },
         output: {
+          entryFileNames: 'js/[name].js',
           manualChunks(id) {
             if (id.includes('node_modules')) {
               return 'vendor';
             }
           },
-          entryFileNames: '[name].js',
         },
       },
       outDir: '../dist',
     },
-    plugins: [injectHTML(), FullReload(['src/**/*.html'])],
+    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
   };
 });
